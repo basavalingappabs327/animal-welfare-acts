4176c7c96a390bd4af3e1ae36d382a80951583d9
basavalingappabs327 <71721682+basavalingappabs327@users.noreply.github.com>
2026-09-10 20:59:52 +0530

    Add files via upload

---
+++ animal welfare act.js
@@ -0,0 +1,115 @@
+
+var array=
+[
+ 
+ ['Animal Birth Control(dogs) Rules 2001','<a href="https://docs.google.com/uc?export=download&id=1L0AuRpt3YOU-nYieY9rIn1ekOMRo_Bwk" download="">click</a>'],
+ 
+ ['Animal Birth Control Rules, 2022','<a href="https://docs.google.com/uc?export=download&id=1dXisX7tpDWipf_pmuyhSfzKJCjt3mZJD" download="">click</a>'],
+ 
+ ['The Karnataka Prevention of Slaughter and Preservation of Cattle Ordinance, 2020','<a href="https://docs.google.com/uc?export=download&id=1RMvBDuQZKj8SQa6B1oXC7uAV8z8lFhug" download="">click</a>'],
+ 
+ ['The Karnataka Prevention of Slaughter and Preservation of Cattle (Transportation Rules),2021','<a href="https://docs.google.com/uc?export=download&id=1XJL-E8MfdPaguXUhFcSj-ZRgNpgBbCxb" download="">click</a>'],
+ 
+ ['Drugs and Cosmetics Act, 1940','<a href="https://docs.google.com/uc?export=download&id=1TzD-KBlrBRa6KM2y9zr6JVl-PbYBOzvV" download="">click</a>'],
+ 
+ ['Drugs, Medical Devices and Cosmetics Bill, 2022','<a href="https://docs.google.com/uc?export=download&id=1Sax8FS8OHU7lAOvkF9RQU5Hu1ldzmZtM" download="">click</a>'],
+ 
+ ['Karnataka Poultry and Livestock Feeds (Regulation of Manufacturer and Sale)1987','<a href="https://docs.google.com/uc?export=download&id=1F99LU4l5uQYuyE8WTKPbxO-p5mWqfeAH" download="">click</a>'],
+ 
+ ['PERFORMING ANIMALS (REGISTRATION) AMENDMENT RULES, 2001','<a href="https://docs.google.com/uc?export=download&id=1OyaCMe0UyuvVzF8EGDUJGQL0UYse8fRm" download="">click</a>'],
+ 
+ ['PERFORMING ANIMALS (REGISTRATION) RULES, 2001','<a href="https://docs.google.com/uc?export=download&id=1NyV7ZDUIb10qx263uYoH-32VAzAUOVS8" download="">click</a>'],
+ 
+ ['PERFORMING ANIMALS RULES, 1973','<a href="https://docs.google.com/uc?export=download&id=1NyV7ZDUIb10qx263uYoH-32VAzAUOVS8" download="">click</a>'],
+
+ ['Prevention and control of infectious And contagious animal disease act 2009','<a href="https://docs.google.com/uc?export=download&id=1prRQC06KK9Y2HQCRmM-kXHz4V6AEb_k4" download="">click</a>'],
+
+ ['The_prevention_of_cruelty_to_animals_act,_1960','<a href="https://docs.google.com/uc?export=download&id=1UO1fEDvGU_sVlIkgn8JksOVMtX51aZly" download="">click</a>'],
+
+ ['Prevention of Cruelty to Animals (Dog Breeding and Marketing) Rules, 2017.','<a href="https://docs.google.com/uc?export=download&id=1EcXO2vPG4fq2XVAlWbRq53VaZEvFXkG6" download="">click</a>'],
+
+ ['Prevention of Cruelty to Animals (ESTABLISHMENT  AND  REGULATION  OF SOCIETIES) RULES, 2001','<a href="https://docs.google.com/uc?export=download&id=1t6vv4Au6-Z4UILz6YLrb5byOIl-11LFR" download="">click</a>'],
+
+ ['Prevention of Cruelty to Animals (SLAUGHTER HOUSE) RULES, 2001','<a href="https://docs.google.com/uc?export=download&id=1GraGTtSJ8EW-3g54i7y4qXTsOfl9Be1t" download="">click</a>'],
+
+ ['PREVENTION OF CRUELTY TO ANIMALS (TRANSPORT OF ANIMALS ON FOOT) RULES, 2001','<a href="https://docs.google.com/uc?export=download&id=13Az63F7ZbTmbU3f5rQNnTu8-ku0Y3CDp" download="">click</a>'],
+
+ ['Prevention of Cruelty to Animals(care and maintenance of case property animals)rules 2016','<a href="https://docs.google.com/uc?export=download&id=1mwQX9FEfgXMP3YkycIXiNQFD7uGwtUvN" download="">click</a>'],
+
+ ['Prevention of Cruelty to Animals(pet shops) rules, 2016','<a href="https://docs.google.com/uc?export=download&id=1azJNbqFyBHEygShFkJRkB1DvcwELrBRY" download="">click</a>'],
+
+ ['THE DRUGS AND COSMETICS RULES, 1945','<a href="https://docs.google.com/uc?export=download&id=1JHHEh7lcEHFhAq0dg7_0atrQfr31c04y" download="">click</a>'],
+
+ ['THE KARNATAKA CATTLE TRESPASS ACT, 1966','<a href="https://docs.google.com/uc?export=download&id=1X-XH2r-S3DmagXP29sKQEEI_GCJu7GgS" download="">click</a>'],
+
+ ['THE KARNATAKA CATTLE TRESPASS RULES, 1971','<a href="https://docs.google.com/uc?export=download&id=1F45bsFn4NV0gkts6dZnKao3DkNVAPBf7" download="">click</a>'],
+
+ ['THE KARNATAKA LIVESTOCK IMPROVEMENT ACT, 1961','<a href="https://docs.google.com/uc?export=download&id=18NT3oVZXxzns0okwbRjxw_ATE9jZaYtf" download="">click</a>'],
+
+ ['THE KARNATAKA LIVESTOCK IMPROVEMENT RULES, 1969','<a href="https://docs.google.com/uc?export=download&id=1EuioolVbNsCUojuyAhQsO31-ExYsShYE" download="">click</a>'],
+
+ ['THE KARNATAKA PREVENTION OF ANIMAL SACRIFICES ACT, 1959','<a href="https://docs.google.com/uc?export=download&id=1F0kb_xlI8kYH-56BrlzbQkegizPzJ2xU" download="">click</a>'],
+
+ ['THE LIVESTOCK IMPORTATION ACT, 1898','<a href="https://docs.google.com/uc?export=download&id=1ErIzRZD5YHxpQQgUDiaggeC9iNp1w-q0" download="">click</a>'],
+ 
+ ['THE LIVESTOCK IMPORTATION(amendment) ACT, 2001','<a href="https://docs.google.com/uc?export=download&id=1Ejlakl9ChSLGZlzDW0sqEQVxHK6VtpHi" download="">click</a>'],
+
+ ['THE PREVENTION OF CRUELTY TO ANIMALS (LICENSING OF FARRIERS) RULES, 1965','<a href="https://docs.google.com/uc?export=download&id=1Lo64987jjjwBhGRUz-bxvRp-HfPgNPvk" download="">click</a>'],
+ 
+ ['THE PREVENTION OF CRUELTY TO ANIMALS (REGISTRATION OF CATTLE PREMISES) RULES,1978','<a href="https://docs.google.com/uc?export=download&id=16m-nlY4hhQjYz7JBLvjZhefFyM5HRX-n" download="">click</a>'],
+ 
+ ['THE PREVENTION OF CRUELTY TO DRAUGHT AND  PACK ANIMALS RULES, 1965','<a href="https://docs.google.com/uc?export=download&id=1AcdzA2PWCHFtzFz9MXkrUJfl70_xEXta" download="">click</a>'],
+ 
+ ['THE WILD LIFE (PROTECTION) ACT, 1972','<a href="https://docs.google.com/uc?export=download&id=1hIb9zsywmtSaX8tl8HD4GKpA4T95z1XL" download="">click</a>'],
+ 
+ ['THE WILD LIFE (PROTECTION)(amendment)ACT, 1991','<a href="https://docs.google.com/uc?export=download&id=1rWtyJsSg_FFeQH69zqk8_LhcSndtRb0d" download="">click</a>'],
+ ['THE WILD LIFE (PROTECTION)(amendment)ACT, 2022','<a href="https://docs.google.com/uc?export=download&id=1ilyRWZ8weArNdVLQTX_N_dxl7sWIZZ2B" download="">click</a>'],
+ 
+ ['bull race/bullock cart race guidelines','<a href="https://docs.google.com/uc?export=download&id=1wb_Al-9hz4rupalAxJQxbVnG2r36RNRG" download="">click</a>'],
+ 
+ ['corrigendum- Bull race','<a href="https://docs.google.com/uc?export=download&id=1w4wbXkCx-l9TuDSyKg-Z0QwoQIGyuaAC" download="">click</a>'],
+ 
+ ['Karnataka Prevention of Slaughter and Preservation of Cattle (Transportation Rules)(amendment), 2021','<a href="https://docs.google.com/uc?export=download&id=1EMit71jgCLvlECPByJXajKHYgtaa3N20" download="">click</a>'],
+ 
+ ['TRANSPORT OF ANIMALS RULES, 1978','<a href="https://docs.google.com/uc?export=download&id=1s4dzbMb_f1zMl4ldHvh9oAomu_s5gDWk" download="">click</a>'],
+ 
+ ['TRANSPORT OF ANIMALS (AMENDMENT) RULES, 2001','<a href="https://docs.google.com/uc?export=download&id=1SIlVjIArFoi6gLyjkVwX8ITtjDWsOxx7" download="">click</a>'],
+ 
+ ['Transport of Animals (Amendment) Rules, 2009','<a href="https://docs.google.com/uc?export=download&id=198hFYI8qFkwKwMpTnbglL_S9txcLRECA" download="">click</a>'],
+ ['National livestock policy, 2013','<a href="https://docs.google.com/uc?export=download&id=1Q0J09Wi0koXMOOOL8xNSkZUFu2mTBLFO" download="">click</a>']
+
+
+]
+
+//for looping the contents of array
+ table=document.getElementById("table");
+ for(var i=0; i< array.length; i++){
+     var newRow=table.insertRow(table.length);
+     for( var j=0;j <array[i].length; j++){
+         var cell=newRow.insertCell(j);
+         cell.innerHTML=array[i][j];
+     }
+ }
+
+ // for search and filter option
+ function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
